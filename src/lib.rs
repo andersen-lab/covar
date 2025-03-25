8cdcaa1c4d14d108f6d7b599d131badfcb15b57f
dylanpilz <dpilz@ucsd.edu>
2025-03-25 12:38:39 -0400

    feat: basic arg parsing, add sars reference data

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,38 @@
-use covar::Cli;
+use std::{error::Error, fs, io::{self, BufReader}};
+use std::env;
+use std::path::Path;
+use clap::Parser;
 
-pub fn run(args: Cli);
+#[derive(Parser)]
+pub struct Cli {
+    #[arg(short = 'i', long = "input")]
+    pub input_bam: std::path::PathBuf,
+
+    #[arg(short = 'r', long = "reference")]
+    pub reference_fasta: std::path::PathBuf,
+
+    #[arg(short = 'a', long = "annotation")]
+    pub annotation_gff: std::path::PathBuf,
+}
+
+pub fn run(args: Cli) -> Result<(), Box<dyn Error>> {
+    let input_bam = fs::File::open(&args.input_bam)
+        .map_err(|e| format!("Failed to open input BAM file: {}", e))?;
+    
+    let reference_fasta = fs::File::open(&args.reference_fasta)
+        .map_err(|e| format!("Failed to open reference FASTA file: {}", e))?;
+    
+    let annotation_gff = fs::File::open(&args.annotation_gff)
+        .map_err(|e| format!("Failed to open annotation GFF file: {}", e))?;
+    
+    // Create BufReaders for more efficient reading (optional)
+    let input_bam_reader = BufReader::new(input_bam);
+    let reference_fasta_reader = BufReader::new(reference_fasta);
+    let annotation_gff_reader = BufReader::new(annotation_gff);
+    
+    println!("Successfully opened all files");
+    
+    // Process the files here...
+    
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,5 @@
-use std::process;
 use clap::Parser;
-
-
-#[derive(Parser)]
-struct Cli {
-    #[arg(short = 'i', long = "input")]
-    input_bam: std::path::PathBuf,
-
-    #[arg(short = 'r', long = "reference")]
-    reference_fasta: std::path::PathBuf,
-
-    #[arg(short = 'a', long = "annotation")]
-    annotation_gff: std::path::PathBuf,
-}
+use covar::{Cli, run};
 
 fn main() {
     let args = Cli::parse();
@@ -20,5 +7,6 @@ fn main() {
     println!("input: {:?}, reference: {:?}, annotation: {:?}",
     args.input_bam, args.reference_fasta, args.annotation_gff);
 
-    
+
+    run(args);
 }
