6c2404df6f373d2a7639830c121cf14c3a1ea3be
dylanpilz <dpilz@ucsd.edu>
2025-09-23 07:49:50 -0700

    chore: use sars 2 metadata files instead of mocking

--- src/utils.rs
+++ src/utils.rs
@@ -131,34 +131,24 @@ mod tests {
 
     #[test]
     fn test_read_reference() {
-        let mock_reference_str = b">chr1\nACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGT";
-        let mock_reference_file = tempfile::NamedTempFile::new().unwrap();
-        std::fs::write(mock_reference_file.path(), mock_reference_str).unwrap();
+        let reference = read_reference(&PathBuf::from("tests/data/NC_045512_Hu-1.fasta")).unwrap();
 
-        let reference = read_reference(&PathBuf::from(mock_reference_file.path())).unwrap();
-
-        assert_eq!(reference.id(), "chr1");
-        assert_eq!(reference.seq().len(), 80);
+        assert_eq!(reference.id(), "NC_045512.2");
+        assert_eq!(reference.seq().len(), 29903);
     }
 
     #[test]
     fn test_read_annotation() {
-        let mock_gff_str = b"##gff-version 3\nchr1\t.\tCDS\t1\t1000\t.\t+\t0\tID=cds1;gene=gene1\nchr1\t.\tCDS\t2000\t3000\t.\t+\t0\tID=cds2;gene=gene2\nchr1\t.\tCDS\t4000\t5000\t.\t+\t0\tID=cds3;gene=ORF1ab\n";
-        let mock_gff_file = tempfile::NamedTempFile::new().unwrap();
-        std::fs::write(mock_gff_file.path(), mock_gff_str).unwrap();
+        let gene_regions = read_annotation(&PathBuf::from("tests/data/NC_045512_Hu-1.gff")).unwrap();
 
-        let gene_regions = read_annotation(&PathBuf::from(mock_gff_file.path())).unwrap();
-
-        assert_eq!(gene_regions.len(), 4);
-        assert_eq!(gene_regions.get(&(1, 1000)).unwrap(), "gene1");
-        assert_eq!(gene_regions.get(&(2000, 3000)).unwrap(), "gene2");
+        assert_eq!(gene_regions.len(), 12);
         assert_eq!(gene_regions.get(&(266, 13468)).unwrap(), "ORF1a");
         assert_eq!(gene_regions.get(&(13468, 21555)).unwrap(), "ORF1b");
     }
 
     #[test]
     fn test_read_bam_and_coverage() {
-        let mock_bam_path = PathBuf::from("tests/data/mock.bam");
+        let mock_bam_path = PathBuf::from("tests/data/test.bam");
         let mut bam = IndexedReader::from_path(&mock_bam_path).unwrap();
         let read_pairs = read_pair_generator(&mut bam, "NC_045512.2", 23000, 24000);
         let coverage_map = get_coverage_map(&read_pairs);
